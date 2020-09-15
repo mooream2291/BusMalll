7918717655696d1bbcbcbc707e805a33e925b8e2
Ashley Moore <mooream2291@gmail.com>
2020-09-14 20:23:10 -0400

    adds

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-'use strict'
+'use strict';
 
 
 //create array/loop to run through images
 
 //create images array
-var images = []
 
-var uservotes = 0
+Item.allItems = [];
+var numOfClicks= [];
+var imagesClicked= [];
+
+var imageIndex= document.getElementById('images');
+var leftContainer= document.getElementById('left');
+var centerContainer= document.getElementById('center');
+var rightContainer= document.getElementById('right');
+
+var uservotes = 0;
 
 function randomindex(max) {
     return Math.floor(Math.random()*Math.floor(max))
@@ -16,10 +24,9 @@ function randomindex(max) {
 function Item (name, source) {//this is your object
     this.name = name;
     this.image = source;
-    this.votes = 0;
-    this.timesShown = 0;
-    this.numOfClicks= [];
-    this.imagesClicked= [];
+    this.clicks = 0;
+    this.views = 0;
+    Item.allItems.push(this);
 }
 //may need to add a third parameter that would be an extension to target image source type this.src = `../img/${src}.jpg`; (this is hardcode to source image, this is what the extentsion will be doing the function of)
 
@@ -27,9 +34,8 @@ function Item (name, source) {//this is your object
 
 //create random number generator
 Vote.prototype.randomimages = function() {
-    var generateImages = randomindex(images.length);
+    var generateImages = randomindex(Item.allItems.length);
 }
-
 
 //put images in HTML
 //create event listener for image clicks
@@ -41,23 +47,24 @@ Vote.prototype.randomimages = function() {
 
 //create mall image vars//
 
-new Item=('R2D2 Luggage Bag', './images/bag.jpg');
-new Item=('Banana Slicer', './images/banana.jpg');
-new Item=('Bathroom Ipad Dock', './images/bathroom.jpg');
-new Item=('Toeless Rubber Boots', './images/boots.jpg');
-new Item=('Compact Breakfast', './images/breakfast.jpg');
-new Item=('Meatball Bubblegum', './images/bubblegum.jpg');
-new Item=('Red Aesthetic Chair', './images/chair.jpg');
-new Item=('Cthulhu Action Figure', './images/cthulhu.jpg');
-new Item=('Rare Dragon Meat', './images/dragon.jpg');
-new Item=('Duck Beak Muzzle', './images/dog-duck.jpg');
-new Item=('Cutlery Pens', './images/pen.jpg');
-new Item=('Sweepin Doggy Booties', './images/pet-sweep.jpg');
-new Item=('Pizza Scissors', './images/scissors.jpg');
-new Item=('Shark Sleeping Bag', './images/shark.jpg');
-new Item=('Baby Sweeper', './images/sweep.jpg');
-new Item=('Tauntaun Sleeping Bag', './images/tauntaun.jpg');
-new Item=('Rare Unicorn Meat', './images/unicorn.jpg');
-new Item=('Tentacle USB', './images/usb.jpg');
-new Item=('Self-filling Watering Can', './images/water-can.jpg');
-new Item=('No-spill Wine Glass', './images/wine-glass.jpg');
+new Item('R2D2 Luggage Bag', './images/bag.jpg');
+new Item('Banana Slicer', './images/banana.jpg');
+new Item('Bathroom Ipad Dock', './images/bathroom.jpg');
+new Item('Toeless Rubber Boots', './images/boots.jpg');
+new Item('Compact Breakfast', './images/breakfast.jpg');
+new Item('Meatball Bubblegum', './images/bubblegum.jpg');
+new Item('Red Aesthetic Chair', './images/chair.jpg');
+new Item('Cthulhu Action Figure', './images/cthulhu.jpg');
+new Item('Rare Dragon Meat', './images/dragon.jpg');
+new Item('Duck Beak Muzzle', './images/dog-duck.jpg');
+new Item('Cutlery Pens', './images/pen.jpg');
+new Item('Sweepin Doggy Booties', './images/pet-sweep.jpg');
+new Item('Pizza Scissors', './images/scissors.jpg');
+new Item('Shark Sleeping Bag', './images/shark.jpg');
+new Item('Baby Sweeper', './images/sweep.jpg');
+new Item('Tauntaun Sleeping Bag', './images/tauntaun.jpg');
+new Item('Rare Unicorn Meat', './images/unicorn.jpg');
+new Item('Tentacle USB', './images/usb.jpg');
+new Item('Self-filling Watering Can', './images/water-can.jpg');
+new Item('No-spill Wine Glass', './images/wine-glass.jpg');
+
